8aa5b34c0a6f2d4de3371a5f8c568d0b4ab560d6
Tens1des <romankotov3213@gmail.com>
2025-09-29 18:16:39 +0300

    Initial Commit

---
+++ Mind Ping/Mind_PingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_PingApp.swift
+//  Mind Ping
+//
+//  Created by Рома Котов on 29.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct Mind_PingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
